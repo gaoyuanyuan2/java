eb5eb32aaa647610f3487025cce84e51828e138e
gaoyuanyuan2 <823789010@qq.com>
2018-06-26 23:47:02 +0800

    StringBuilder 方法链

--- src/main/java/com/study/java/oop/other.java
+++ src/main/java/com/study/java/oop/other.java
@@ -3,5 +3,12 @@ package com.study.java.oop;
 public class other {
     public static void main(String[] args) {
         String a = new String("a");//两个对象，a指向常量池"a"
+
+        //底层是char[]数组，方法链
+        StringBuilder sb = new StringBuilder();   //字符数组长度初始为16
+        StringBuilder sb1 = new StringBuilder(32);   //字符数组长度初始为32
+        StringBuilder sb2 = new StringBuilder("abcd");   //字符数组长度初始为32, value[]={'a','b','c','d',\u0000,\u0000...}
+        sb2.append("efg");
+        sb2.append(true).append(321).append("随便");   //通过return this实现方法链.
     }
 }
