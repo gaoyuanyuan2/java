d9c06061ee1f85a8a9beddd75bb8eac61986775c
gaoyuanyuan2 <823789010@qq.com>
2018-07-13 23:42:01 +0800

    打印系统信息，包括编码信息

---
+++ src/main/java/com/study/java/io/others/Encoding.java
@@ -0,0 +1,8 @@
+package com.study.java.io.others;
+
+public class Encoding {
+    //打印系统信息，包括编码信息
+    public static void main(String[] args) {
+        System.getProperties().list(System.out);
+    }
+}
