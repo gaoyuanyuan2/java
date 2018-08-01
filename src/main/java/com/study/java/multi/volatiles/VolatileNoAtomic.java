ea392c5447897bd5e4722d54592377e79ed14f41
gaoyuanyuan2 <823789010@qq.com>
2018-08-01 23:49:42 +0800

    CAS

--- src/main/java/com/study/java/multi/volatiles/VolatileNoAtomic.java
+++ src/main/java/com/study/java/multi/volatiles/VolatileNoAtomic.java
@@ -9,6 +9,12 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class VolatileNoAtomic extends Thread{
 	//private static volatile int count;
+	/*
+	* CAS,compare and swap的缩写，中文翻译成比较并交换。比同步锁效率高
+	*CAS 操作包含三个操作数 —— 内存位置（V）、预期原值（A）和新值(B)。
+	*如果内存位置的值与预期原值相匹配，那么处理器会自动将该位置值更新为新值 。否则，处理器不做任何操作
+	*
+	* */
 	private static AtomicInteger count = new AtomicInteger(0);
 	private static void addCount(){
 		for (int i = 0; i < 1000; i++) {
