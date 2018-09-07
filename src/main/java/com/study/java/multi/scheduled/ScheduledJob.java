0f7c3ecbe16abbf3173da32f5ea1fe13ec1b2bc0
yanguowei <yanguow@133.cn>
2018-09-07 11:31:00 +0800

    pool、ScheduledJob

--- src/main/java/com/study/java/multi/scheduled/ScheduledJob.java
+++ src/main/java/com/study/java/multi/scheduled/ScheduledJob.java
@@ -1,5 +1,6 @@
 package com.study.java.multi.scheduled;
 
+import java.io.IOException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -20,12 +21,16 @@ public class ScheduledJob {
 
         ScheduledFuture<?> scheduleTask = scheduler.scheduleWithFixedDelay(command, 5, 1, TimeUnit.SECONDS);
 
+        scheduler.schedule(() -> System.out.println("延时执行"), 1000 * 60 * 10, TimeUnit.MILLISECONDS);
+
+
         //定时器线程池
-        Executors.newScheduledThreadPool(3).scheduleAtFixedRate(
-                () ->  System.out.println("bombing!"),
-                6,
-                2,
-                TimeUnit.SECONDS);
+        Executors.newScheduledThreadPool(3).
+                scheduleAtFixedRate(
+                        () -> System.out.println("bombing!"),
+                        6,
+                        2,
+                        TimeUnit.SECONDS);
     }
 
 }
