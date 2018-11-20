351f5c95c77e11d3f5bc27aa1400c219114fd106
yanguowei <yanguow@133.cn>
2018-11-20 11:25:15 +0800

    动态代理获取字节码内容

--- src/main/java/com/study/java/gof23/proxy/myDynamicProxy/Client.java
+++ src/main/java/com/study/java/gof23/proxy/myDynamicProxy/Client.java
@@ -1,12 +1,22 @@
 package com.study.java.gof23.proxy.myDynamicProxy;
 
+import sun.misc.ProxyGenerator;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.reflect.Proxy;
 
 public class Client {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		RealStar realStar = new RealStar();
 		StarHandler handler = new StarHandler(realStar);
 		Star proxy = (Star)Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(),  new Class[]{Star.class} , handler);
 		proxy.sing();
+
+		//获取字节码内容
+		byte[] data = ProxyGenerator.generateProxyClass("$Proxy0",new Class[]{Star.class});
+		FileOutputStream os = new FileOutputStream("D:/$Proxy0.class");
+		os.write(data);
+		os.close();
 	}
 }
