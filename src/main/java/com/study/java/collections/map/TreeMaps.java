0aedaa375ef67c055218250addefe82dab68119b
yanguowei <yanguow@133.cn>
2019-08-22 17:55:45 +0800

    雪花算法

---
+++ src/main/java/com/study/java/collections/map/TreeMaps.java
@@ -0,0 +1,22 @@
+package com.study.java.collections.map;
+
+import java.util.HashMap;
+import java.util.TreeMap;
+
+/**
+ * Created by yan on  22/08/2019.
+ */
+public class TreeMaps {
+
+
+
+    public static void main(String[] args) {
+        //红黑树
+        TreeMap<String,String> treeMap = new TreeMap();
+
+        HashMap<String,String> hashMap = new HashMap();
+
+        System.out.println(1 << 30);
+
+    }
+}
