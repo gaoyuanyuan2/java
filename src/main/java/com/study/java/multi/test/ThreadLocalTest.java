1df414d6726fb6fb2f7751d5ec9c18b126810919
gaoyuanyuan2 <823789010@qq.com>
2019-11-30 17:30:42 +0800

    juc

---
+++ src/main/java/com/study/java/multi/test/ThreadLocalTest.java
@@ -0,0 +1,28 @@
+package com.study.java.multi.test;
+
+public class ThreadLocalTest {
+
+    /**
+     * 对于以下代码，thread1 中设置 threadLocal 为 1，而 thread2 设置 threadLocal 为 2。
+     * 过了一段时间之后，thread1 读取 threadLocal 依然是 1，不受 thread2 的影响。
+     * */
+    public static void main(String[] args) {
+        ThreadLocal threadLocal = new ThreadLocal();
+        Thread thread1 = new Thread(() -> {
+            threadLocal.set(1);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(threadLocal.get());
+            threadLocal.remove();
+        });
+        Thread thread2 = new Thread(() -> {
+            threadLocal.set(2);
+            threadLocal.remove();
+        });
+        thread1.start();
+        thread2.start();
+    }
+}
--- src/main/java/com/study/java/multi/volatiles/RunThread.java
+++ src/main/java/com/study/java/multi/volatiles/RunThread.java
@@ -1,30 +1,52 @@
 package com.study.java.multi.volatiles;
 
-public class RunThread extends Thread{
+public class RunThread implements Runnable {
 
-	private volatile boolean isRunning = true;
-	private void setRunning(boolean isRunning){
-		this.isRunning = isRunning;
-	}
-	
-	public void run(){
-		System.out.println("into..");
-		int i = 0;
-		while(isRunning == true){
-			//..
-			System.out.println("1");
-		}
-		System.out.println("stop");
-	}
-	
-	public static void main(String[] args) throws InterruptedException {
-		RunThread rt = new RunThread();
-		rt.start();
-		Thread.sleep(1000);
-		rt.setRunning(false);
-		System.out.println("isRunning set false");
-		Thread.sleep(100000);
-	}
-	
-	
+    /**
+     *
+     在虚拟机的Client模式下，由于JIT并没有做足够的优化，在主线程修改ready变量的状态后，ReaderThread 可以发现这个改动，
+     并退出程序。但是在Server 模式下，由于系统优化的结果，ReaderThread 线程无法“看到”主线程中的修改，
+     导致ReaderThread永远无法退出( 因为代码第7行判断永远不会成立)，这显然不是我们想看到的结果。这个问题就是一个典型的可见性问题。
+
+     注意:可以使用Java虚拟机参数server切换到Server模式。
+
+     *
+     **/
+
+    private boolean isRunning = true;
+
+    private void setRunning(boolean isRunning) {
+        this.isRunning = isRunning;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("into..");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        isRunning = false;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        RunThread rt = new RunThread();
+        new Thread(rt).start();
+
+        while (true) {
+            if (rt.isRunning()) {
+                System.out.println("run");
+            } else {
+                System.out.println("stop");
+                return;
+            }
+        }
+    }
+
+
 }
